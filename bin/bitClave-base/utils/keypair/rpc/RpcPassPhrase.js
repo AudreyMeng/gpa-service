3914e7619d5f32685a6322da3970d123e821359f
Anton Batalin <antbat2009@gmail.com>
2018-09-22 18:14:18 +0300

    update bin

--- bin/bitClave-base/utils/keypair/rpc/RpcPassPhrase.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class RpcPassPhrase {
-    constructor(pass) {
-        this.pass = pass;
-    }
-}
-exports.default = RpcPassPhrase;
-//# sourceMappingURL=RpcPassPhrase.js.map
